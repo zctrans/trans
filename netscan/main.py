e2fed051bced88a236c74911e1918da5717dcd1b
zctrans <zocwu113@gmail.com>
2022-12-19 18:30:43 +0300

    latest

--- netscan/main.py
+++ netscan/main.py
@@ -1,41 +1,37 @@
 import scapy.all as scapy
-from tabulate import tabulate
 import argparse
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses')
+    options = parser.parse_args()
 
-class Caught:
-    def __init__(self, ip, mac):
-        self.ip = ip
-        self.mac = mac
+    if not options.target:
 
-    def show(self):
-        print(f"\n------------------------\nIP: {self.ip}\nMAC: {self.mac}\n------------------------\n")
-
-
+        parser.error("[-] Please specify an IP Address or Addresses, use --help for more info.")
+    return options
+  
 def scan(ip):
+    arp_req_frame = scapy.ARP(pdst = ip)
 
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
+    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     
-    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    
-    caughts = []
+    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
 
-    for el in answered:
-        for answer in el:
-            caughts.append(Caught(answer.pdst, answer.hwsrc))
+    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+    result = []
+    for i in range(0,len(answered_list)):
+        client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
+        result.append(client_dict)
 
-    return caughts
-        
-def show_tabled(list_of_caught):
-    print(tabulate(
-        [[caught.ip, caught.mac] for caught in list_of_caught],
-        headers=['IP', 'MAC']
-    ))    
+    return result
+  
+def display_result(result):
+    print("-----------------------------------\nIP Address\tMAC Address\n-----------------------------------")
+    for i in result:
+        print("{}\t{}".format(i["ip"], i["mac"]))
+  
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('-t', type=str, dest='target', help='IP of victim')
-args = argparser.parse_args()
-
-show_tabled(scan(args.target))
+options = get_args()
+scanned_output = scan(options.target)
+display_result(scanned_output)
